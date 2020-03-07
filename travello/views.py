d5f6f1ccacc5c16709ca29c4a1e74d6513b1fbd9
Trisha Singhal <singhaltrisha25@gmail.com>
2020-03-08 01:57:05 +0800

    practice

--- travello/views.py
+++ travello/views.py
@@ -9,12 +9,14 @@ def index(request):
     dest1.desc = 'The City never sleeps!'
     dest1.img = 'destination_1.jpg'
     dest1.price  = 1000
+    dest1.offer  = False
     
     dest2 = Destination()
     dest2.name = 'Agra'
     dest2.desc = 'The City never sleeps!'
     dest2.img = 'destination_2.jpg'
     dest2.price  = 800
+    dest2.offer = True
 
     dests = [dest1, dest2]
 
